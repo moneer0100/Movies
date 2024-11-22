368f9f47f3a41c94232ff868b48efc56b56192cf
moneer ahmed <moneerahmed14@gmail.com>
2024-11-22 16:43:28 +0200

    creating login screen anf register screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.components)
+    implementation(libs.androidx.activity)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
---
+++ app/src/main/java/com/example/movieapp/Register.kt
@@ -0,0 +1,24 @@
+package com.example.movieapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.movieapp.databinding.ActivityRegisterBinding
+
+class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.haveAccount.setOnClickListener{
+            val intent=Intent(this,Login::class.java)
+            startActivity(intent)
+        }
+        
+        }
+    }
